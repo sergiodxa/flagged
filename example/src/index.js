70642febe75b37eee51814644ae09fd90811ca21
Hermes <jack-ten@hotmail.com>
2021-09-29 13:16:55 -0300

    Add renderFallback cases

--- example/src/index.js
+++ example/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { FlagsProvider, withFeature, Feature, useFeature } from '../../src/index';
+import { FlagsProvider, withFeature, Feature, useFeature } from 'flagged'; //#TODO: change to represent local version.
 
 function VersionComponent({ version }) {
   return <h1>You are currently in {version}.</h1>;
@@ -18,8 +18,9 @@ const V2Component = withFeature('v2')(function V2Component() {
   return <VersionComponent version="v2" />;
 });
 
-function V3Component() {
-  return <VersionComponent version="v3" />;
+function V3Component(props) {
+  const version = `v3 | from ${props.from}`;
+  return <VersionComponent version={version} />;
 }
 
 function V4Component() {
@@ -49,15 +50,32 @@ function App() {
 
       <V1Component />
 
-      <V2Component />
+      {/* Render Prop */}
+      <Feature name="v2" render={<V2Component />}/>
 
-      {/* Render and Render Fallback Prop */}
-      <Feature name="v3" render={<V3Component />} renderFallback={<p>You are not in V3.</p>} />
+      {/* Render Prop */}
+      <Feature name="v2" render={(isEnabled) =>
+          isEnabled ? <V2Component/> : <h3>You are not in V2</h3>
+      }/>
+
+
+      {/* Render and RenderFallback Prop */}
+      <Feature name="v3" render={<V3Component from="render prop"/>} renderFallback={<h3>You are not in V3| fallback from render prop.</h3>} />
+
+      {/* Children and RenderFallback Prop */}
+      <Feature name="v3" renderFallback={<h3>You are not in V3| fallback from children prop.</h3>}>
+        <V3Component from="children prop"/>
+      </Feature>
+
+      {/*Only Fallback Prop */}
+      <Feature name="v3" renderFallback={<h3>You are not in V3| only fallback.</h3>}>
+      </Feature>
 
       {/* Function as Children */}
       <Feature name="v4">
         {hasV4 => (hasV4 ? <V4Component /> : <h3>You are not in v4.</h3>)}
       </Feature>
+
     </FlagsProvider>
   );
 }
