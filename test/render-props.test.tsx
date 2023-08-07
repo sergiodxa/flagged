5432c4f5a7af268fca641366e8ec11f60e7537d3
Sergio Xalambrí <hello@sergiodxa.com>
2023-08-07 13:02:47 -0500

    Switch to Vitest

--- test/render-props.test.tsx
+++ test/render-props.test.tsx
@@ -1,4 +1,3 @@
-import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
 import * as React from 'react';
 import { Feature, FlagsProvider } from '../src';
